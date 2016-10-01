163e24c87618707fb808fd1174dc83b310f6b3ef
Jess Rudolph <jrudol4@students.towson.edu>
2016-10-01 17:31:03 -0500

    JavaDocs & Refactored Code

--- src/CharacterMultiples.java
+++ src/CharacterMultiples.java
@@ -1,4 +1,5 @@
 /**
+ * @author Jess Rudolph
  * 
  */
 
@@ -8,6 +9,17 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Map;
 
+
+/**
+ * Generates 2 parallel arrays: a string array and an int array. 
+ * The string array contains strings of characters, symbols, 
+ * numbers, and punctuation, while the int array provides an 
+ * integer that represents a multiple of characters in the 
+ * corresponding string. The output is an integer array that 
+ * counts the number of unique characters of a string that 
+ * are a multiple of the multiple integer that was input.
+ *
+ */
 public class CharacterMultiples {
     
 	public static final int MAX_ARRAY = 100000;
@@ -31,15 +43,16 @@ public class CharacterMultiples {
 		stringLength = stringOfChars.length;
 		multiplesLength = multiplesArray.length;
 		
-		System.out.println("String Length: " +stringLength);
-		System.out.println("Multiples Length: " +multiplesLength); 	
+		//System.out.println("String Length: " +stringLength);
+		//System.out.println("Multiples Length: " +multiplesLength); 	
 		
 		compareArrayLengths(stringLength, multiplesLength);
 		countCharacters(stringOfChars, multiplesArray);
+	    System.out.println(actualMultipleCount);
 	}
 
 	/**
-	 * This method will be modified to accept input from the keyboard
+	 * @return the string array
 	 */
 	public static String[] getStringArray(){
 		stringOfChars = new String[] {"aababcabcd","aababcabcd", "$$YUD99 996025NSOn doi%%%%"};
@@ -47,13 +60,22 @@ public class CharacterMultiples {
 	}
 	
 	/**
-	 * 
+	 * @return the integer array of multiples
 	 */
 	public static int[] getMultiplesArray(){
 		multiplesArray = new int[] {3, 2, 2};
 		return multiplesArray;
 	} 
 	
+	/**
+	 * Compare the lengths of the integer array and the string array.
+	 * Ensure that the arrays are not null.
+	 * Ensure that the arrays have the same number of indices.
+	 * Ensure that the length of the arrays does not exceed 100,000 indices. 
+	 * 
+	 * @param stringLength		the length of the string array
+	 * @param multiplesLength	the length of the int array
+	 */
 	public static void compareArrayLengths(int stringLength, int multiplesLength){
 		if (stringLength == 0 || multiplesLength == 0){
 			System.out.println("One or both of your arrays are empty.");
@@ -71,7 +93,19 @@ public class CharacterMultiples {
 		}
 	}
 	
-	public static void countCharacters(String[] strings, int[] nums) {
+
+	/**
+	 * Counts the number of occurrences of each unique character in the string at multiplesArray[i].
+	 * If the number of occurrences is a multiple of the number entered in multiplesArray[i],
+	 * it is added to the count in the integer array.
+	 * 
+	 * @param stringOfChars		the string array 
+	 * @param multiplesArray	the multiple array 
+	 * @return					the ArrayList of integers for the number of times a character
+	 * 							in string, multiplesArray[i], was a multiple of the integer
+	 * 							in the corresponding multiplesArray.
+	 */
+	public static ArrayList<Integer> countCharacters(String[] stringOfChars, int[] multiplesArray) {
 		int multiple = 0;
 		String tempChars = "";
 		
@@ -101,7 +135,7 @@ public class CharacterMultiples {
 		    }
 		    actualMultipleCount.add(count);
 		}
-	    System.out.println(actualMultipleCount);
+	    return actualMultipleCount;
 	}
 	
 }
